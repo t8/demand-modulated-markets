1f9dc1be0d96af01a1f8c4c146cef39656d3130f
Tate Berenbaum <tateflyer1@gmail.com>
2022-07-07 11:45:28 -0400

    feat: sell functionality

--- src/contract.js
+++ src/contract.js
@@ -2,12 +2,14 @@
 
 export function handle(state, action) {
   const calledFunction = action.input.function;
+  const caller = action.caller;
   let balances = state.balances;
   if (calledFunction === "buy") {
-    // Mint tokens to curve
+    // Mint tokens to curve //
     
     // TODO: Validate amount of transfer
     let amount = 100;
+
     while (amount !== 0) {
       let supply = getCurrentSupply(state);
       let priceToMint = calcPrice(supply);
@@ -16,16 +18,55 @@ export function handle(state, action) {
         balances[caller]++;
         amount -= priceToMint;
       } else if (amount < priceToMint) {
-        // TODO: Add to foreignCalls to return rest of amount
+        // Add to foreignCalls to return rest of amount
+        state.foreignCalls.push({
+          tdID: SmartWeave.transaction.id,
+          contract: state.pair,
+          input: {
+            function: "transfer",
+            target: caller,
+            qty: amount
+          }
+        });
       }
     }
-  } else if (calledFunction === "sell") {
-    // Burn tokens and return funds
+   
+    state.balances = balances;
+    return { state };
 
-    
+  } else if (calledFunction === "sell") {
+    // Burn tokens and return funds //
+
+    let amount = action.input.qty;
+    let returns = 0;
+    while (amount !== 0) {
+      let supply = getCurrentSupply(state);
+      let burnPrice = calcPrice(supply);
+
+      if (amount >= burnPrice && amount - burnPrice >= 0) {
+        balances[caller]--;
+        amount -= burnPrice;
+        returns += burnPrice;
+      }
+    }
+
+    // Add to foreignCalls to give tokens back to user
+    state.foreignCalls.push({
+      txID: SmartWeave.transaction.id,
+      contract: state.pair,
+      input: {
+        function: "transfer",
+        target: caller,
+        qty: returns
+      }
+    });    
+
+    state.balances = balances;
+    return { state };
 
   } else if (calledFunction === "transfer") {
-    // Transfer tokens to another wallet
+    // Transfer tokens to another wallet //
+    
     const target = input.target
     const qty = input.qty
 
@@ -55,6 +96,7 @@ export function handle(state, action) {
       balances[target] = qty;
     }
 
+    state.balances = balances;
     return { state };
   }
 }
